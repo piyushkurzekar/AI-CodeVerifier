d28b13e8acabb927a578370e7cf9554117d00761
piyushkurzekar <piyushkurzekar8390@gmail.com>
2025-09-22 12:07:36 +0530

    deleted

--- backend/src/routes/ai.routes.js
+++ backend/src/routes/ai.routes.js
@@ -1,13 +1,7 @@
 const express = require ('express')
 const aiController = require ("../controllers/ai.controller")
-const bcrypt = require ('bcrypt')
 const router = express.Router();
 
 router.post("/get-review", aiController.getReview)
-
-// const 
-
-
-
 
 module.exports= router;
