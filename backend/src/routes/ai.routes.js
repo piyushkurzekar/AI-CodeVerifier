e4a54088bf13d254bd91e2f643a319d657a180eb
piyushkurzekar <piyushkurzekar8390@gmail.com>
2025-09-22 12:05:31 +0530

    my commit

--- backend/src/routes/ai.routes.js
+++ backend/src/routes/ai.routes.js
@@ -1,11 +1,13 @@
 const express = require ('express')
 const aiController = require ("../controllers/ai.controller")
-
+const bcrypt = require ('bcrypt')
 const router = express.Router();
 
 router.post("/get-review", aiController.getReview)
 
 // const 
+
+
 
 
 module.exports= router;
